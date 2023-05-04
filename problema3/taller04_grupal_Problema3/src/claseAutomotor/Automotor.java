faede19f5b0ed6a6aad9939f3050dc16db26e02f
Richard Ortiz <soy.nahomi.mucho.gusto@gmail.com>
2023-05-04 11:52:58 -0500

    Problemas listos 2.0

--- problema3/taller04_grupal_Problema3/src/claseAutomotor/Automotor.java
+++ problema3/taller04_grupal_Problema3/src/claseAutomotor/Automotor.java
@@ -9,5 +9,83 @@ package claseAutomotor;
  * @author SALA I
  */
 public class Automotor {
+    // Un automotor tiene entre sus características, cédula del dueño,
+    // marca de vehículo, año de fabricación, valor de vehículo y valor de la 
+    // matricula. El valor de la matricula es igual al 0.002% del valor de 
+    // vehículo por el número de años de antiguedad del vehiculo.
+    
+    private String cedula;
+    private String marcaV;
+    private int anio;
+    private double valorV;
+    private double valorM;
+    
+    public Automotor(){
+        cedula = "1150057501";
+        marcaV = "Mitsubishi";
+        anio = 2017;
+        valorV = 15000.00;
+    }
+    
+    public Automotor(String cd, String mV, int a, double precio){
+        cedula = cd;
+        marcaV = mV;
+        anio = a;
+        valorV = precio;
+    }
+    
+    public void establecerCedula(String c){
+        cedula = c;
+    }
+    
+    public void establecerMarcaVehiculo(String m){
+        marcaV = m;
+    }
+    
+    public void establecerAnio(int n){
+        anio = n;
+    }
+    
+    public void establecerValorVehiculo(double n){
+        valorV = n;
+    }
+    
+    // public void establecerValorMatricula(double n){
+    public void calcularValorMatricula(){
+        valorM = (0.00002*valorV)*(2023-anio);
+    }
+    
+    public String obtenerCedula(){
+        return cedula;
+    }
+    
+    public String obtenerMarcaVehiculo(){
+        return marcaV;
+    }
+    
+    public int obtenerAnio(){
+        return anio;
+    }
+    
+    public double obtenerValorVehiculo(){
+        return valorV;
+    }
+    
+    public double obtenerValorMatricula(){
+        return valorM;
+    }
+    
+    @Override
+    public String toString(){
+       String cadena = String.format("Los datos del Automotor son:\n"
+                        +" Cedula del propietario: %s\n"
+                        +" Marca Vehiculo: %s\n"
+                        +" Año de fabricacion: %d\n"
+                        +" Valor del Vehiculo: %.2f\n"
+                        +" Valor de la Matricula: %.2f\n",cedula,
+                        marcaV,anio,
+                        valorV,valorM);
+       return cadena;
+    }
     
 }
--- problema3/taller04_grupal_Problema3/src/ejecutor/Ejecutor.java
+++ problema3/taller04_grupal_Problema3/src/ejecutor/Ejecutor.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ejecutor;
-
+import claseAutomotor.Automotor;
 /**
  *
  * @author SALA I
@@ -14,7 +14,18 @@ public class Ejecutor {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Automotor vehiculo1 = new Automotor();
+       
+       vehiculo1.calcularValorMatricula();
+       
+       System.out.printf("%s\n", vehiculo1);
+       
+       Automotor vehiculo2 = new Automotor("1100005790","Datsun",1990,30000.50);
+       
+       vehiculo2.calcularValorMatricula();
+       
+       System.out.printf("%s\n", vehiculo2);
     }
     
 }
